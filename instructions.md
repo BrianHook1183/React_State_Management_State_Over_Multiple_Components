48948979d544b7411005a8ebe7944cda41c22356
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2021-04-12 00:18:07 -0500

    added instructions

---
+++ instructions.md
@@ -0,0 +1,6 @@
+# Instructions
+
+Add a button to the header which increases the font size of the content. Initialize the font size to 12 and increase it by 2 with each button click.
+
+### Specific Instructions
+The button should be after the login button
--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,15 @@ import React, { useState } from "react";
 import Content from "./Content";
 import Header from "./Header";
 
+
+/* 
+Add a button to the header which increases the font size of the content. Initialize the font size to 12 and increase it by 2 with each button click.
+
+*Specific Instructions
+The button should be after the login button
+*/
+
+
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
   const toggleLoggedIn = () => setLoggedIn(!loggedIn);
